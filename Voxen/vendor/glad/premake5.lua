8569b9efd3343a36384d818720be3a1d7e58a14c
loganr3093 <loganr3093@gmail.com>
2024-06-24 19:48:46 -0700

    Update Premake for static linking

--- Voxen/vendor/glad/premake5.lua
+++ Voxen/vendor/glad/premake5.lua
@@ -2,7 +2,6 @@ project "glad"
     kind "StaticLib"
     language "C"
     staticruntime "on"
-    warnings "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -34,17 +33,6 @@ project "glad"
         runtime "Debug"
         symbols "on"
 
-    filter { "system:windows", "configurations:Debug-AS" }
-        runtime "Debug"
-        symbols "on"
-        sanitize { "Address" }
-        flags { "NoRuntimeChecks", "NoIncrementalLink" }
-
     filter "configurations:Release"
         runtime "Release"
-        optimize "speed"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "speed"
-        symbols "off"
+        optimize "on"
