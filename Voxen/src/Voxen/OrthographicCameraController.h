b5254dfcb87af01f550a6cad8a9b2019c6e40cf7
loganr3093 <loganr3093@gmail.com>
2024-07-14 12:24:49 -0700

    Add getters and setters to camera controller

--- Voxen/src/Voxen/OrthographicCameraController.h
+++ Voxen/src/Voxen/OrthographicCameraController.h
@@ -19,6 +19,21 @@ namespace Voxen
 		OrthographicCamera& GetCamera() { return m_Camera; }
 		const OrthographicCamera& GetCamera() const { return m_Camera; }
 
+		void SetZoomLevel(float level) { m_ZoomLevel = level; }
+		float GetZoomLevel() { return m_ZoomLevel; }
+
+		void SetRotationSpeed(float speed) { m_CameraRotationSpeed = speed; }
+		float GetRotationSpeed() { return m_CameraRotationSpeed; }
+
+		void SetTranslationSpeed(float speed) { m_CameraTranslationSpeed = speed; }
+		float GetTranslationSpeed() { return m_CameraTranslationSpeed; }
+
+		void SetPosition(glm::vec3 pos) { m_CameraPosition = pos; }
+		glm::vec3 GetPosition() { return m_CameraPosition; }
+
+		void SetRotation(float rot) { m_CameraRotation = rot; }
+		float GetRotation() { return m_CameraRotation; }
+
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
@@ -31,6 +46,7 @@ namespace Voxen
 
 		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
 		float m_CameraTranslationSpeed = 5.0f;
+
 		float m_CameraRotation = 0.0f;
 		float m_CameraRotationSpeed = 90.0f;
 	};
