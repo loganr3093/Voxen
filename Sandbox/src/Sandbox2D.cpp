1d4fae34f177ec0caad4e29b1b7699a84c4e39c9
loganr3093 <loganr3093@gmail.com>
2024-07-21 23:52:11 -0700

    Add profiling instrumentation and visual profiling

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -6,43 +6,6 @@
 
 #include "Platform/OpenGL/OpenGLShader.h"
 
-template<typename Fn>
-class Timer
-{
-public:
-	Timer(const char* name, Fn&& func) : m_Name(name), m_Stopped(false), m_Func(func)
-	{
-		m_StartTimepoint = std::chrono::high_resolution_clock::now();
-	}
-
-	~Timer()
-	{
-		if (!m_Stopped) Stop();
-	}
-
-	void Stop()
-	{
-		auto endTimepoint = std::chrono::high_resolution_clock::now();
-
-		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-
-		m_Stopped = true;
-
-		float duration = (end - start) * 0.001f;
-
-		m_Func({ m_Name, duration });
-	}
-
-private:
-	const char* m_Name;
-	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
-	bool m_Stopped;
-	Fn m_Func;
-};
-
-#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) { m_ProfileResults.push_back(profileResult); })
-
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
 {
@@ -59,11 +22,11 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Voxen::Timestep ts)
 {
-	PROFILE_SCOPE("Sandbox2D::OnUpdate");
+	VOX_PROFILE_FUNCTION();
 
 	// Update
 	{
-		PROFILE_SCOPE("CameraController::OnUpdate");
+		VOX_PROFILE_SCOPE("CameraController::OnUpdate");
 		m_CameraController.OnUpdate(ts);
 	}
 
@@ -83,17 +46,10 @@ void Sandbox2D::OnUpdate(Voxen::Timestep ts)
 
 void Sandbox2D::OnImGuiRender()
 {
+	VOX_PROFILE_FUNCTION();
+
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
-
-	for (auto& result : m_ProfileResults)
-	{
-		char label[50];
-		strcpy(label, "%.3fms  ");
-		strcat(label, result.Name);
-		ImGui::Text(label, result.Time);
-	}
-	m_ProfileResults.clear();
 
 	ImGui::End();
 }
