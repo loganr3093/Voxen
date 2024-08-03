7457db22985da037e74dd3fa77048c5fe6255f3c
loganr3093 <loganr3093@gmail.com>
2024-08-03 09:07:38 -0700

    Update bind event funciton

--- Voxen/src/Voxen/Core/Core.h
+++ Voxen/src/Voxen/Core/Core.h
@@ -66,7 +66,7 @@
 #endif
 
 #define BIT(x) (1 << x)
-#define VOX_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define VOX_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Voxen
 {
