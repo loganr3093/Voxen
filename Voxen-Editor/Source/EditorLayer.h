f97a3fa301f2bd4206a097a7935aca2df9a420c5
loganr3093 <loganr3093@gmail.com>
2024-09-02 08:18:40 -0700

    Add Play Button

--- Voxen-Editor/Source/EditorLayer.h
+++ Voxen-Editor/Source/EditorLayer.h
@@ -30,6 +30,12 @@ namespace Voxen
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
 	private:
 		OrthographicCameraController m_CameraController;
 
@@ -53,8 +59,19 @@ namespace Voxen
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0,
+			Play = 1
+		};
+
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor Resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
